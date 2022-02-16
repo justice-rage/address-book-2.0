2ab91edc0a12bb5ab1b33c5d9607b99b6a2f3c5b
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-15 20:23:24 -0800

    add multiple address input functionality
    Co-authored-by: Michael Shamrell <michaelshamrell2001@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -32,12 +32,33 @@ AddressBook.prototype.deleteContact = function(id)  {
   return true;
 };
 
+//Busiess Logic for EmailAddresses
+
+function EmailAddresses(workEmailAddress, personalEmailAddress) {
+  this.workEmailAddress = workEmailAddress;
+  this.personalEmailAddress = personalEmailAddress;
+};
+
 // business logic for Contacts
-function Contact(firstName, lastName, phoneNumber)  {
+function Contact(firstName, lastName, phoneNumber, emailAddresses, physicalAddress)  {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
-}
+  this.emailAddresses = emailAddresses;
+  this.physicalAddress = physicalAddress;
+  // this.emailAddresses = {
+  //    this.mainEmailAddress = mainEmailAddress,
+  //    this.secondEmailAddress = secondEmailAddress,
+  //    this.thirdEmailAddress = thirdEmailAddress,
+     //this.emailAddresses = {
+     //workEmailAdress:  
+   };
+
+
+
+EmailAddresses.prototype.allEmails = function() {
+return "Work Email: " + this.workEmailAddress + " Personal Email: " + this.personalEmailAddress;
+};
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
@@ -62,6 +83,9 @@ function showContact(contactId) {
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
+  $(".email-address").html(contact.emailAddress)
+  $(".physical-address").html(contact.physicalAddress)
+  //(".")
   let buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
@@ -85,14 +109,28 @@ $(document).ready(function() {
     const inputtedFirstName = $("input#new-first-name").val();
     const inputtedLastName = $("input#new-last-name").val();
     const inputtedPhoneNumber = $("input#new-phone-number").val();
+    const inputtedWorkEmailAddress = $("input#new-work-email-address").val();
+    const inputtedPersonalEmailAddress = $("input#new-personal-email-address").val();
+    const inputtedPhysicalAddress = $("input#new-physical-address").val();
 
     // The next three lines are new:
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
+    $("input#new-work-email-address").val("");
+    $("input#new-personal-email-address").val("");
+    $("input#new-physical-address").val("");
 
-    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    let newEmailAdresses = new EmailAddresses(inputtedWorkEmailAddress, inputtedPersonalEmailAddress);
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, newEmailAdresses, inputtedPhysicalAddress);
     addressBook.addContact(newContact);
+    console.log(newContact.emailAddresses.allEmails(newEmailAdresses))
+    newContact.emailAddresses.allEmails(newEmailAdresses)
     displayContactDetails(addressBook);
+    console.log(EmailAddresses());
   });
 });
+
+// let emailAddresses = {name: "Email Address", emails: []};
+// let workEmailAddress = {name: "Work Email"};
+// emailAddresses.email.push(workemailAddress);
